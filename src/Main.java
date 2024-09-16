57c80545c613ef72557681cec9b7ab503987cf1a
Amy <amyphuongnguyenn@gmail.com>
2024-09-16 00:59:38 -0400

    Completed Part 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numberOfKidsInFamily = 3;
+        boolean isItRaining = false;
+        double priceOfAGallonOfGas = 3.75;
+        int favoriteNumber = 7;
+        double shoeSize = 9.5;
+        int birthMonth = 9;
+        String fullName = "Amy Nguyen";
+
+
+        System.out.println("Number of kids in my family: " + numberOfKidsInFamily);
+        System.out.println("Is it raining? " + isItRaining);
+        System.out.println("Price of a gallon of gas: $" + priceOfAGallonOfGas);
+        System.out.println("My favorite number: " + favoriteNumber);
+        System.out.println("My shoe size: " + shoeSize);
+        System.out.println("My birth month: " + birthMonth);
+        System.out.println("My full name: " + fullName);
+    }
+}
